89996340cde366267aa375253995e8fcf506984f
vhdrjb <panda.soft.group@gmail.com>
2024-08-08 11:53:39 +0330

    fix images

--- lib/infrastructure/extensions/string_extensions.dart
+++ lib/infrastructure/extensions/string_extensions.dart
@@ -3,24 +3,24 @@ import 'dart:html' as html;
 
 extension StringExtensions on String {
   String get toPngImageUrl =>
-      '${html.window.location.origin}/assets/images/png/$this.png';
+      '${html.window.location.origin}/assets/assets/images/png/$this.png';
 
   String get toJpgImageUrl =>
-      '${html.window.location.origin}/assets/images/jpg/$this.jpg';
+      '${html.window.location.origin}/assets/assets/images/jpg/$this.jpg';
 
   String get toProjectImageUrl =>
-      '${html.window.location.origin}/assets/images/projects/$this.png';
+      '${html.window.location.origin}/assets/assets/images/projects/$this.png';
 
   String get toProjectWebPUrl =>
-      '${html.window.location.origin}/assets/images/projects/$this.webp';
+      '${html.window.location.origin}/assets/assets/images/projects/$this.webp';
 
   String get toProjectCoverUrl =>
-      '${html.window.location.origin}/assets/images/projects/covers/$this.webp';
+      '${html.window.location.origin}/assets/assets/images/projects/covers/$this.webp';
 
   String get toNetworkImage =>
-      '${html.window.location.origin}/assets/images/projects/$this';
+      '${html.window.location.origin}/assets/assets/images/projects/$this';
 
-  AssetImage get toPngImage => AssetImage('assets/images/png/$this.png');
+  AssetImage get toPngImage => AssetImage('assets/assets/images/png/$this.png');
 
-  AssetImage get toJpgImage => AssetImage('assets/images/jpg/$this.jpg');
+  AssetImage get toJpgImage => AssetImage('assets/assets/images/jpg/$this.jpg');
 }
